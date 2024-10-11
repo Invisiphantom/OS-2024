6374ed270ced797c9eda6cb056670142431118ec
Ethan Cao <1677035769@qq.com>
2024-10-11 10:47:31 +0000

    recover proc_test.c printk

--- src/test/proc_test.c
+++ src/test/proc_test.c
@@ -26,41 +26,41 @@ static void proc_test_1b(u64 a)
 
     switch (a / 10 - 1) {
 
-    case 0: // pass
-        break;
+        case 0: // pass
+            break;
 
-    // 进行三次调度
-    // 然后退出唤醒root_proc
-    case 1:
-        yield();
-        yield();
-        yield();
-        break;
+        // 进行三次调度
+        // 然后退出唤醒root_proc
+        case 1:
+            yield();
+            yield();
+            yield();
+            break;
 
-    case 2: // pass
-        post_sem(&s1);
-        break;
+        case 2: // pass
+            post_sem(&s1);
+            break;
 
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-        if (a & 1)
-            post_sem(&s2);
-        else
-            wait_sem(&s2);
-        break;
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+            if (a & 1)
+                post_sem(&s2);
+            else
+                wait_sem(&s2);
+            break;
 
-    case 8:
-        wait_sem(&s3);
-        post_sem(&s4);
-        break;
+        case 8:
+            wait_sem(&s3);
+            post_sem(&s4);
+            break;
 
-    case 9:
-        post_sem(&s5);
-        wait_sem(&s6);
-        break;
+        case 9:
+            post_sem(&s5);
+            wait_sem(&s6);
+            break;
     }
 
     exit(a); // 退出码为a
@@ -81,62 +81,62 @@ static void proc_test_1a(u64 a)
 
     switch (a) {
 
-    case 0: { // pass
-        int t = 0, x;
-        // 等待10个子进程全部结束
-        for (int i = 0; i < 10; i++) {
-            wait(&x);
-            t |= 1 << (x - 10);
-        }
-        ASSERT(t == 1023);
-        ASSERT(wait(&x) == -1);
-    } break;
+        case 0: { // pass
+            int t = 0, x;
+            // 等待10个子进程全部结束
+            for (int i = 0; i < 10; i++) {
+                wait(&x);
+                t |= 1 << (x - 10);
+            }
+            ASSERT(t == 1023);
+            ASSERT(wait(&x) == -1);
+        } break;
 
-    case 1: // pass
-        break;
+        case 1: // pass
+            break;
 
-    case 2: { // pass
-        for (int i = 0; i < 10; i++)
-            ASSERT(wait_sem(&s1));
-        // 确保此时信号量为0
-        ASSERT(get_sem(&s1) == false);
-    } break;
+        case 2: { // pass
+            for (int i = 0; i < 10; i++)
+                ASSERT(wait_sem(&s1));
+            // 确保此时信号量为0
+            ASSERT(get_sem(&s1) == false);
+        } break;
 
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7: {
-        int x;
-        // 等待10个子进程全部结束
-        for (int i = 0; i < 10; i++)
-            wait(&x);
-        ASSERT(wait(&x) == -1);
-    } break;
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7: {
+            int x;
+            // 等待10个子进程全部结束
+            for (int i = 0; i < 10; i++)
+                wait(&x);
+            ASSERT(wait(&x) == -1);
+        } break;
 
-    case 8: {
-        int x;
-        for (int i = 0; i < 10; i++)
-            post_sem(&s3);
-        for (int i = 0; i < 10; i++)
-            wait(&x);
-        ASSERT(wait(&x) == -1);
-        ASSERT(s3.val == 0);
-        ASSERT(get_all_sem(&s4) == 10);
-    } break;
+        case 8: {
+            int x;
+            for (int i = 0; i < 10; i++)
+                post_sem(&s3);
+            for (int i = 0; i < 10; i++)
+                wait(&x);
+            ASSERT(wait(&x) == -1);
+            ASSERT(s3.val == 0);
+            ASSERT(get_all_sem(&s4) == 10);
+        } break;
 
-    case 9: {
-        int x;
-        for (int i = 0; i < 10; i++)
-            wait_sem(&s5);
-        for (int i = 0; i < 10; i++)
-            post_sem(&s6);
-        for (int i = 0; i < 10; i++)
-            wait(&x);
-        ASSERT(wait(&x) == -1);
-        ASSERT(s5.val == 0);
-        ASSERT(s6.val == 0);
-    } break;
+        case 9: {
+            int x;
+            for (int i = 0; i < 10; i++)
+                wait_sem(&s5);
+            for (int i = 0; i < 10; i++)
+                post_sem(&s6);
+            for (int i = 0; i < 10; i++)
+                wait(&x);
+            ASSERT(wait(&x) == -1);
+            ASSERT(s5.val == 0);
+            ASSERT(s6.val == 0);
+        } break;
     }
 
     exit(a); // 退出码为a
@@ -169,9 +169,7 @@ static void proc_test_1()
         int code, id;
         id = wait(&code);
         ASSERT(pid[code] == id);
-#ifdef DEBUG
-        printk(">>>>>>>> proc_test_1a-case %d exit <<<<<<<<\n\n", code);
-#endif
+        printk("proc %d exit\n", code);
     }
 
     exit(0); // 退出码为0
